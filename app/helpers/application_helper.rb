4f79a758d9aea8f402b6677f5989d5a9942af7e7
Karla Holman <holmankarla@gmail.com>
2015-06-29 09:53:51 -0700

    Styled the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	
+	def flash_class(flashtype)
+		case flashtype
+			when "alert"
+				"alert-warning"
+			when "notice"
+				"alert-info"
+			else
+				""
+		end
+	end
+
 end
